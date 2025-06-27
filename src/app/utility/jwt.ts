75087724ac20f94c040fc893d1368495f6f43745
mehediemon1437 <mdmehedihasanemon1437@gmail.com>
2025-06-27 18:50:08 +0600

    second Commit

--- src/app/utility/jwt.ts
+++ src/app/utility/jwt.ts
@@ -1,16 +1,19 @@
-import jwt from 'jsonwebtoken';
+import jwt, { SignOptions } from 'jsonwebtoken';
 import config from '../config';
 
-const JWT_SECRET = config.jwt_secret;
-console.log(JWT_SECRET);
+// Ensure JWT_SECRET is defined and typed as string
+const JWT_SECRET: string = config.jwt_secret as string;
 if (!JWT_SECRET) {
   throw new Error('JWT_SECRET is not defined in environment variables');
 }
 
-export const generateToken = (payload: object, expiresIn = '7d') => {
+export const generateToken = (
+  payload: object,
+  expiresIn: SignOptions['expiresIn'] = '7d'
+): string => {
   return jwt.sign(payload, JWT_SECRET, { expiresIn });
 };
 
-export const verifyToken = (token: string) => {
+export const verifyToken = (token: string): any => {
   return jwt.verify(token, JWT_SECRET);
 };
